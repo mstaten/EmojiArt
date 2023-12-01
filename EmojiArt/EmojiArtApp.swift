ea4bab95a984a275388cf41179b24111580b1464
Michelle <mnstaten@yahoo.com>
2023-12-01 13:23:55 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Michelle Staten on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
